fa358907a53c436c5d3da8524734f185816fe82b
Alexander <lazeratus1@gmail.com>
2023-06-09 19:38:40 -0400

    learned hidden functions and functions within functions still dont fully understand

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(funActivity = `roller-skate`) {
+    return `This Saturday, I want to ${funActivity}!`
+}
+saturdayFun(`bathe my dog`)
+
+const mondayWork = function(backToWork = `go to the office`) {
+    return `This Monday, I will ${backToWork}.`
+}
+mondayWork(`work from home`)
+
+/*function wrapAdjective(congrats){
+    const party1 = `*a hard worker*!`;
+    return function() {
+        const party2 = `||a dedicated programmer||!`
+        return function() {
+            return (`${congrats}`)
+        }
+    }
+}*/
+
+  function wrapAdjective(symbol = `*`) {
+    return function(adjective = `special`) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
